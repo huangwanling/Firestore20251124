264a4e394d151f460fd1bc3516f558f2997122ed
huangwanling <huangwanling154@gmail.com>
2025-11-24 15:47:50 +0800

    v2新增資料(自動產生文件ID)

--- app/src/main/java/tw/edu/pu/csim/tcyang/firestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/firestore/MainActivity.kt
@@ -1,5 +1,6 @@
 package tw.edu.pu.csim.tcyang.firestore
 
+import UserScoreScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirestoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                UserScoreScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirestoreTheme {
-        Greeting("Android")
     }
 }
